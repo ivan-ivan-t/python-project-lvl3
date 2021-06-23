affa09a2a4767e6f2248caa8565604b259b47b6d
mand <ivterehov@gmail.com>
2021-06-23 22:48:11 +0300

    update scripts

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,12 +1,17 @@
 import argparse
+import os
+
+from page_loader.engine import download
 
 
 def main():
     parser = argparse.ArgumentParser(description='page-loader')
     parser.add_argument('url', type=str)
-    parser.add_argument('-o', '--output', help='set path for output', type=str)
+    parser.add_argument('-o', '--output', help='set path for output', type=str,
+                        default=os.getcwd())
     args = parser.parse_args()
-
+    
+    print(download(args.url, args.output))   
 
 if __name__ == '__main__':
     main()
