cde91b12f358776acd44a6089eb19fbb4b0f4cbb
mand <ivterehov@gmail.com>
2021-06-23 11:13:34 +0300

    test

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,11 +1,9 @@
-import argparse
-
-
 def main():
-    parser = argparse.ArgumentParser(description='page-loader')
-    parser.add_argument('url', type=str)
-    parser.add_argument('-o', '--output', help='set path for output', type=str)
-    args = parser.parse_args()
+    print('sss')
+#    parser = argparse.ArgumentParser(description='page-loader')
+#    parser.add_argument('url', type=str)
+#    parser.add_argument('-o', '--output', help='set path for output', type=str)
+#    args = parser.parse_args()
 
 
 if __name__ == '__main__':
